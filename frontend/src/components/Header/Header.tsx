2b221e3524d1164492decd4f2cbb700a549e9f1d
Mikhail Myshakin <myshakinm@mail.ru>
2025-06-18 14:59:23 +0300

    create Header component

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import classes from "./Header.module.css";
+
+export const Header = () => {
+    return (
+        <div className={classes.header}>
+            <div>some text</div>
+            <div>CSV Аналитик CSV Генератор История</div>
+        </div>
+    );
+};
