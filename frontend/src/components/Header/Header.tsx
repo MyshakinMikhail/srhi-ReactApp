5b1cdb7ab0095045854f9c5360d1af9e100339d0
Mikhail Myshakin <myshakinm@mail.ru>
2025-06-18 23:44:42 +0300

    replace directory pictures into Header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
-import { Generator } from "../pictures/Menu/Routers/Generator";
-import { GeneratorCurr } from "../pictures/Menu/Routers/GeneratorCurr";
-import { History } from "../pictures/Menu/Routers/History";
-import { HistoryCurr } from "../pictures/Menu/Routers/HistoryCurr";
-import { Uploader } from "../pictures/Menu/Routers/Uploader";
-import { UploaderCurr } from "../pictures/Menu/Routers/UploaderCurr";
-import { SummerSchoolLogo } from "../pictures/SummerSchoolLogo";
-import { Tesis } from "./../pictures/Menu/Tesis/Tesis";
 import classes from "./Header.module.css";
+import { Generator } from "./pictures/Menu/Routers/Generator";
+import { GeneratorCurr } from "./pictures/Menu/Routers/GeneratorCurr";
+import { History } from "./pictures/Menu/Routers/History";
+import { HistoryCurr } from "./pictures/Menu/Routers/HistoryCurr";
+import { Uploader } from "./pictures/Menu/Routers/Uploader";
+import { UploaderCurr } from "./pictures/Menu/Routers/UploaderCurr";
+import { Tesis } from "./pictures/Menu/Tesis/Tesis";
+import { SummerSchoolLogo } from "./pictures/SummerSchoolLogo";
 
 export const Header = ({ important }: { important: string }) => {
     let uploader: boolean = false;
