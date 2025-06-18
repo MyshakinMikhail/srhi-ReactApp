4a17d279d3b27c77116c5615e2bc181171a9c088
Mikhail Myshakin <myshakinm@mail.ru>
2025-06-18 15:00:35 +0300

    added DownloadPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { DownloadPage } from "./pages/DownloadPage/DownloadPage";
+
 export default function App() {
-    return <div>Мое приложение</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<DownloadPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
