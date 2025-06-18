658993f9add5cd335564bdfc0bc1cee632ddb087
Mikhail Myshakin <myshakinm@mail.ru>
2025-06-18 16:46:50 +0300

    added header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,22 @@
+import { Generator } from "../pictures/Menu/Generator";
+import { Uploader } from "../pictures/Menu/Uploader";
+import { SummerSchoolLogo } from "../pictures/SummerSchoolLogo";
+import { History } from "./../pictures/Menu/History";
+import { Tesis } from "./../pictures/Menu/Tesis/Tesis";
 import classes from "./Header.module.css";
 
 export const Header = () => {
     return (
         <div className={classes.header}>
-            <div>some text</div>
-            <div>CSV Аналитик CSV Генератор История</div>
+            <div className={classes.leftPart}>
+                <SummerSchoolLogo />
+                <Tesis />
+            </div>
+            <div className={classes.menu}>
+                <Uploader />
+                <Generator />
+                <History />
+            </div>
         </div>
     );
 };
